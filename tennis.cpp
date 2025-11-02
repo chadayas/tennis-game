6f84d4c61ec7ab9a9235aeea61a38dd22cf3f0f4
chadayas <cgassoya@gmail.com>
2025-11-02 16:22:06 -0500

    fixed stretching rectangle

--- tennis.cpp
+++ tennis.cpp
@@ -21,14 +21,25 @@ struct Ball {
 	bool begin;
 };
 
+struct TopBorder {
+	Vector2 position;
+	Vector2 size;
+};
+
+struct BottomBorder {
+	Vector2 position;
+	Vector2 size;
+};
 
 // Window size
 
-	const int width= 1200;
-	const int height = 1000;
+	const int screen_width= 1200;
+	const int screen_height = 1000;
 	
-	const char t[]= "MY Window";
-
+	const char t[]= "Tennis Game";
+	const char start[] = "Welcome";
+	const char press[] = "Press SPACE to start";
+	
 	// For our two rectangles
 	const float l_recStart = 100;
 	const float r_recStart = 1100;
@@ -39,46 +50,57 @@ struct Ball {
 	
 	// For our circle
 	float radius = 15.0;
-	const float pos_x = width / 2;
-	const float pos_y = height / 2;
+	const float center_x = screen_width / 2;
+	const float center_y = screen_height / 2;
 	const float move_x = 5;
 	const float move_y = 5;
+	int fontSize = 15;
 	
+	bool moveBall = false;
+
+
 	// name for our structs
 	
 	leftRectangle l_rec;
 	leftRectangle r_rec;
 	Ball ball;
+	TopBorder top_border;
+	BottomBorder bottom_border;
 	
 	// movement vectors
 
 	Vector2 ballMovement = { move_x, move_y };	
 	Vector2 recMovement = { move_x, move_y };	
 
-
+	void StartScreen(){
+		
+		DrawText(start, center_x, center_y, fontSize, RAYWHITE);
+	}
 
 	void PlayerMovement(){
 		if (IsKeyDown(KEY_UP)) l_rec.position.y -= recMovement.y;
 		if (IsKeyDown(KEY_DOWN)) l_rec.position.y += recMovement.y;
 	}
-	void ComputerMovement();
+	void ComputerMovement(){
+
+
+	}
 
 	
 	
 	void BallMovement(){
-		if (IsKeyPressed(KEY_P)){
-			ball.position.x += 2 * ballMovement.x;
-			ball.position.y +=  ballMovement.y;
-
-		}			
+	  	ball.position.x +=- 2 * ballMovement.x;
+		ball.position.y +=  ballMovement.y;
+	
 	}
+
 
 int main(){
 
 
-	InitWindow(width, height, t);
+	InitWindow(screen_width, screen_height, t);
 	
-		// vectors
+	// rectangles, ball, and border init
 	
 	l_rec.position = Vector2{ l_recStart, middle };
 	r_rec.position = Vector2{ r_recStart, middle }; 
@@ -86,7 +108,13 @@ int main(){
 	l_rec.size = Vector2{rec_width, rec_height};
 	r_rec.size = Vector2{rec_width, rec_height};
 	
- 	ball.position = Vector2{ pos_x, pos_y };
+	bottom_border.position = Vector2{1,screen_height - 20};
+	bottom_border.size = Vector2{screen_width, rec_width};	
+	
+	top_border.position = Vector2{1,1};
+	top_border.size = Vector2{screen_width, rec_width};
+
+ 	ball.position = Vector2{ center_x, center_y};
 	ball.radius = radius;	
 		
 
@@ -94,25 +122,69 @@ int main(){
 
 
 	while(!WindowShouldClose()){
-	
-		// Moving our rectangle
-		// Right now our rectangle streches when key is pressed
-		// when instead we want a static rectangle
-	
-		// move the circle 
 		
 		ClearBackground(BLACK);
+  	
+		Rectangle player_rec = {l_rec.position.x, l_rec.position.y
+			,l_rec.size.x, l_rec.size.y };
+		
+		Rectangle computer_rec = {r_rec.position.x, r_rec.position.y
+			, r_rec.size.x, r_rec.size.y };
+		
+		Rectangle topBorder = {top_border.position.x, top_border.position.y
+			,top_border.size.x, top_border.size.y };
+		
+		Rectangle bottomBorder = {bottom_border.position.x, bottom_border.position.y
+			,bottom_border.size.x, bottom_border.size.y };
 
+
+
+
+		// move the circle 
+		
+		if (IsKeyPressed(KEY_SPACE)) moveBall = true;
+		else if (IsKeyPressed(KEY_P)) moveBall = false;
+
+		if (moveBall){
+			ball.position.x +=- 2 * ballMovement.x;
+			ball.position.y +=  ballMovement.y;
+
+			r_rec.position.y += ballMovement.y;
+		}
+	
+			// Ball collision logic
+		
+		if (CheckCollisionCircleRec(ball.position, ball.radius, player_rec))  
+			ballMovement.x *= -1;
+		
+		if (CheckCollisionCircleRec(ball.position, ball.radius, topBorder))
+			ballMovement.y *= -1;
+	
+		if (CheckCollisionCircleRec(ball.position, ball.radius, bottomBorder))
+			ballMovement.y *= -1;
+		if (CheckCollisionCircleRec(ball.position, ball.radius, computer_rec))  
+			ballMovement.x *= -1;
+		
+
+
+		//if (CheckCollisionRecs(bottomBorder, computer_rec) ||
+		//CheckCollisionRecs(topBorder, computer_rec))
+		//	r_rec.position.y *= -1;	
+		
+		
 		BeginDrawing();
-			
+
 		PlayerMovement();
 		
-		BallMovement();
 
 		DrawRectangleV(l_rec.position, l_rec.size, RAYWHITE);
 		DrawRectangleV(r_rec.position, r_rec.size, RAYWHITE);
-
 	
+		// top and bottom borders
+		
+		DrawRectangleV(top_border.position, top_border.size,RAYWHITE);
+		DrawRectangleV(bottom_border.position, bottom_border.size,RAYWHITE);
+		
 		DrawCircleV(ball.position, ball.radius, RAYWHITE);
 
 		EndDrawing();
